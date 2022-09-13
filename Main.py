2f259423f48dcc1427f4428e8eb85b28015c4663
URK21CS1178 <110010623+URK21CS1178@users.noreply.github.com>
2022-09-13 11:27:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ def insertionSort(array) -> List[int]:
         j = step - 1     
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
-            j = j - 1
+            j = j - 1  
         
         array[j + 1] = key
    return array
